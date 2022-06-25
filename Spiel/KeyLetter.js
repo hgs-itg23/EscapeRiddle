fe72614c440f6b31a05cdc450cb8e374c57da090
BlackArroww <78801797+BlackArroww@users.noreply.github.com>
2022-06-25 21:22:00 +0200

    GitHub Fehlerbehebung

--- Spiel/KeyLetter.js
+++ Spiel/KeyLetter.js
@@ -2,19 +2,9 @@ function storage(){
     let a = localStorage.setItem('letter', keyletter);
 }
 function ready(){
-<<<<<<< HEAD
-    document.getElementsById('keyletter').value = a;
-}
-=======
 
     document.getElementById(keyletter).value = letter;
     
 }
 
-function storage (){
 
-    localStorage.setItem(letter, keyletter);
-
-}
-
->>>>>>> b0d275a51125cc9d95b98a41e31266312e38eddb
