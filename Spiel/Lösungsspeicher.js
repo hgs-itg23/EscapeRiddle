9eca1dc57947a5c512614cb870556b739953196a
Asusgamer40 <78801504+Asusgamer40@users.noreply.github.com>
2022-07-02 16:38:08 +0200

    menueSeite QOL

--- Spiel/Lösungsspeicher.js
+++ Spiel/Lösungsspeicher.js
@@ -1,12 +1,12 @@
 function solved1(){
     if(localStorage.getItem('solved1') == 'true'){
         var b = localStorage.getItem('letter');
-        document.querySelector(".done1").innerHTML = b;
+        document.querySelector("#done1").innerHTML = b;
     }
 }
 function solved2(){
     if(localStorage.getItem('solved2') == 'true'){
         var c = localStorage.getItem('letter2');
-        document.querySelector(".done2").innerHTML = c;
+        document.querySelector("#done2").innerHTML = c;
     }
 }
