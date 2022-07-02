790596436477e8e112777e842fcda6cc7546a62d
Asusgamer40 <78801504+Asusgamer40@users.noreply.github.com>
2022-07-02 15:34:24 +0200

    MenueSeite Buttons zu Schlüsselwörter hinzugefügt

--- Spiel/KeyLetter.js
+++ Spiel/KeyLetter.js
@@ -1,11 +1,10 @@
-function storage(){
-    localStorage.setItem('letter', keyletter);
-}
-function displaystart(){
-
-}
 function keyletterinput(){
-    var b = localStorage.getItem('letter');
-    document.getElementById('keyletter').innerHTML = b;
-    
+    if(localStorage.getItem('solved1') == 'true'){
+        var b = localStorage.getItem('letter');
+        document.getElementById('keyletter').innerHTML = b;
+    }
+    if(localStorage.getItem('solved2') == 'true'){
+        var b = localStorage.getItem('letter2');
+        document.getElementById('keyletter').innerHTML = b;
+    }
 }
