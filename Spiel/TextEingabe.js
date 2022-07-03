4e0950c5a01a2794cb2afb3e4284caf5ed576a5e
Asusgamer40 <78801504+Asusgamer40@users.noreply.github.com>
2022-07-03 09:38:40 +0200

    enterkey mechanik überarbeitet

--- Spiel/TextEingabe.js
+++ Spiel/TextEingabe.js
@@ -1,20 +1,22 @@
-
 function check(){
-    if(document.getElementById('Lösung').value == Lösung || document.getElementById('Lösung').value == Lösung2){
+    if(document.querySelector('#Lösung').value == Lösung){
         window.location.href= 'RichtigSeite.html';
     }
     else{
-        document.getElementById('Lösung').value = 'Falsch';
+        document.querySelector('#Lösung').value = 'Falsch';
     }
 }
 function reset(){
-        document.getElementById('Lösung').value = '';
+        document.querySelector('#Lösung').value = '';
 }
 function master(){
     if(document.querySelector('#MasterLösung').value =='Kairo'){
         window.location.href = 'MasterRichtigSeite.html';
     }
+    else{
+        document.querySelector('#MasterLösung').value = 'Falsch';
+    }
 }
 function resetmaster(){
-    document.getElementById('MasterLösung').value = '';
+    document.querySelector('#MasterLösung').value = '';
 }
