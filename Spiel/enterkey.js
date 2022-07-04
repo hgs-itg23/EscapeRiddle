342092a3933a41dcce41394034beb2b7441e7031
Asusgamer40 <78801504+Asusgamer40@users.noreply.github.com>
2022-07-04 17:00:20 +0200

    enterkey.js funktioniert jetzt überall

--- Spiel/enterkey.js
+++ Spiel/enterkey.js
@@ -5,6 +5,15 @@ function enter(){
         if(checkmode == '1'){
             check();
         }
+        else if(checkmode == '2'){
+            window.location.href = 'Schlüsselwörter.html';
+        }
+        else if(checkmode == '3'){
+            window.location.href = 'MenueSeite.html';
+        }
+        else if(checkmode == '4'){
+            window.location.href = 'Dankeseite.html';
+        }
         else{
             master();
         }
